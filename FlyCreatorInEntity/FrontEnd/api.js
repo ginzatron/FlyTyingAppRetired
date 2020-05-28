88fd4fa10851a7ae4cbee5aa11ca22c79e931ec4
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-05-28 14:48:06 -0500

    trying to work on auth. adding some functionality to front end

--- FlyCreatorInEntity/FrontEnd/api.js
+++ FlyCreatorInEntity/FrontEnd/api.js
@@ -1,23 +1,73 @@
 const url = "https://localhost:44329/api";
 
-const getFlysButton = document.querySelector("#getFlys");
-getFlysButton.addEventListener('click',GetFlys);
+const FlysButton = document.querySelector("#getFlys");
+FlysButton.addEventListener('click',GetFlys);
 
-const getFlyButton = document.querySelector("#getFly");
-getFlyButton.addEventListener('click',GetFly);
+const RegisterButton = document.querySelector("#register");
+RegisterButton.addEventListener('click',Register);
+
+const getFlyForm = document.querySelector(".getFlyForm");
+getFlyForm.addEventListener('submit',GetFly)
 
 
+async function Login(){
+    const logInUrl = "/admin/login";
 
-async function GetFlys(){
-    const flyUrl = "/fly";
+    const response = await fetch(url+logInUrl, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "email": "njginsburg@gmail.com",
+            "password": "Test123!",
+            "rememberMe": false
+        })
+    });
 
-    const response = await fetch(url+flyUrl);
     const data = await response.json();
     console.log(data);
 }
 
-async function GetFly(id="11"){
-    const flyUrl = `/fly/11`;
+async function Register(){
+    const registerUrl = "/admin/register";
+
+    const response = await fetch(url+registerUrl, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "email": "ekginsburg@gmail.com",
+            "password": "Test123!",
+            "confirmPassword": "Test123!"
+        })
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+}
+
+async function GetFlys(){
+    const flyUrl = "/fly";
+    const flyContainer = document.querySelector(".flyContainer");
+
+
+    const response = await fetch(url+flyUrl);
+    const data = await response.json();
+    console.log(data);
+
+    data.forEach(fly => {
+        flyContainer.innerHTML += `<div>${fly.name} id:${fly.id}</div><div>${fly.classification.classification}</div>`
+    })
+
+}
+
+async function GetFly(event){
+    event.preventDefault();
+    const flyId = event.currentTarget.flyId.value;
+    const flyUrl = `/fly/${flyId}`;
 
     const response = await fetch(url+flyUrl);
     const data =await response.json();
