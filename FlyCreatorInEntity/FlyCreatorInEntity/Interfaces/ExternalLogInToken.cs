b0be55456463928a73c25950c69dd7d1071ed58a
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-05-31 16:18:16 -0500

    Admin controller takes ExternalLogInToken inheriting from IExternalLogInToken interface as opposed to provider specific token because it's always just a string

---
+++ FlyCreatorInEntity/FlyCreatorInEntity/Interfaces/ExternalLogInToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyCreator.Interfaces
+{
+    public class ExternalLogInToken : IExternalLogInToken
+    {
+        public string Id_Token { get; set; }
+    }
+}
