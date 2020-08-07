a15b83b0d83250408b888af057da22cd5b591e28
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-08-07 08:26:29 -0500

    removed local LogIn and Register from front end. Will not be using local storage for users credentials, only profile information

--- FlyCreatorInEntity/FlyCreatorInEntity/Controllers/AdminController.cs
+++ FlyCreatorInEntity/FlyCreatorInEntity/Controllers/AdminController.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; 
 using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Update;
